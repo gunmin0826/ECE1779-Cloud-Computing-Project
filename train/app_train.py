e521404ff7a66e4f35c309cefc165f5ba0a6cf73
yidakang <yida.kang@mail.utoronto.ca>
2024-03-17 17:22:53 -0400

    Organised the files, each task is now in its own folder

--- train/app_train.py
+++ train/app_train.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 def train_model():# Load a model
-    model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
+    model = YOLO('..\\yolov8n.pt')  # load a pretrained model (recommended for training)
 
     results = model.train(data='coco128.yaml', epochs=100, imgsz=640)   
     return results
